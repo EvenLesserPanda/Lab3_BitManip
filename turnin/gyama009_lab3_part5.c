47213e408be849ee1d4e37f72e9c275098a92371
Garrett Yamano <gyama009@wch136-25.cs.ucr.edu>
2020-04-12 16:48:43 -0700

    First Commit

---
+++ turnin/gyama009_lab3_part5.c
@@ -0,0 +1,27 @@
+/*	Author: gyama009
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+    	while (1) {
+		PORTC = ((PINA & 0x80) >> 7) + ((PINA & 0x40) >> 6) + ((PINA & 0x20) >> 5) +
+			 ((PINA & 0x10) >> 4) + ((PINA & 0x08) >> 3) + ((PINA & 0x04) >> 2) +
+			 ((PINA & 0x02) >> 1) + (PINA & 0x01) + ((PINB & 0x80) >> 7) +
+			 ((PINB & 0x40) >> 6) + ((PINB & 0x20) >> 5) + ((PINB & 0x10) >> 4) +
+			 ((PINB & 0x08) >> 3) + ((PINB & 0x04) >> 2) + ((PINB & 0x02) >> 1) + (PINB & 0x01); 
+	}
+	return 0;
+}
